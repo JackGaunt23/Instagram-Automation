a0de2772e3c4bcc206ab25dbb8c9d207cde02d59
JackGaunt23 <jackg2355@gmail.com>
2024-08-11 12:40:19 -0700

    Testing video upload

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ require("dotenv").config();
 
 const { IgApiClient } = require('instagram-private-api');
 const { get } = require('request-promise');
+const fs = require('fs');
 
 const postToInsta = async () => {
     const ig = new IgApiClient();
     ig.state.generateDevice(process.env.IG_USERNAME);
     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 
-    const imageBuffer = await get({
-        url: 'https://i.pinimg.com/originals/f3/0f/6a/f30f6acfafbf5b47d760f97ed7b4a99f.jpg',
-        encoding: null, 
-    });
+    const videoBuffer = fs.readFileSync("C:/Users/jackg/Videos/letthingsbe_testvid.mp4");
+    const coverImage = fs.readFileSync("C:/Users/jackg/Downloads/Screenshot 2024-08-11 123506.jpg");
 
-    await ig.publish.photo({
-        file: imageBuffer,
-        caption: 'Start doing!',
-    });
+    const videoOptions = {
+        video: videoBuffer,
+        coverImage: coverImage,
+        caption: 'Let things be',
+    };
+
+    await ig.publish.video(videoOptions);
 }
 
 postToInsta();
